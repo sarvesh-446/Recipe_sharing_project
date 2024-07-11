7057978551c18cb264be0bc77790671c19f96f97
sarvesh-446 <sarveshpadmanaban013@gmail.com>
2024-07-11 10:17:37 +0530

    Update recipes.js

--- server/src/router/recipes.js
+++ server/src/router/recipes.js
@@ -1,33 +1,14 @@
 const express = require("express");
 const recipeRouter = express.Router();
-const multer = require("multer");
-const path = require("path");
+
 const recipeController = require("../controllers/recipe.js");
 
-// const storage = multer.diskStorage({
-// 	destination: (req, file, cb) => {
-// 		cb(
-// 			null,
-// 			path.join(__dirname, "../../../client/recipe-app/public/images")
-// 		);
-// 	},
-// 	filename: (req, file, cb) => {
-// 		cb(
-// 			null,
-// 			file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-// 		);
-// 	},
-// });
-
-// const upload = multer({
-// 	storage: storage,
-// });
 
 recipeRouter.get("/", recipeController.getAllRecipes);
 
 recipeRouter.get("/:recipeID", recipeController.getRecipeByID);
 
-// recipeRouter.post("/upload-image", upload.single("file"), recipeController.uploadImage);
+
 
 recipeRouter.post("/post-recipe", recipeController.postRecipe);
 
